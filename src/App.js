e1f061140b6232c362fe76be205b39d6929624ac
Daichi Yoshida <yoshida@jsk.co.jp>
2019-05-24 16:27:28 +0900

    props を使う

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 // コンポーネントをインポート
-import Title from './components/Tiltle';
+import Title from "./components/Tiltle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // イベントハンドラー
+  onClickHandler = () => {
+    // h2タグを取得
+    let title = document.getElementById("versionCounter");
+
+    // upgradeのボタン
+    let upgradeButton = document.getElementById("upgradeButton");
+
+    // タイトルを更新
+    title.textContent = "4.0";
+
+    // pタグ(upgradeのボタン)を非表示
+    upgradeButton.style.display = "none";
+    
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Title
+            title="Hello World 3.0"
+            titleStyle={{ color: "orange" }}
+            onClick={this.onClickHandler}
+          >
+            Hello World{" "}
+            <span
+              id="versionCounter"
+              style={{ borderBottom: "1px solid orange" }}
+            >
+              3.0
+            </span>
+          </Title>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Tiltle.jsx
+++ src/components/Tiltle.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
 // functional component
-const Title = () => {
-    return(
-        <div>
-            <h2>Hello World コンポーネント</h2>
-        </div>
-    )
-}
+const Title = props => {
+  return (
+    <div className="titleSection">
+      <h2 style={props.titleStyle} id="versionStatement">
+        {props.children}
+      </h2>
+      <p onClick={props.onClick} id="upgradeButton">
+        Upgrade
+      </p>
+    </div>
+  );
+};
 
 export default Title;
