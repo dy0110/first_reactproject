152b877b02ad126ab174cb6325d1a89605f1025b
Daichi Yoshida <yoshida@jsk.co.jp>
2019-05-24 16:00:37 +0900

    コンポーネントを作製する

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// コンポーネントをインポート
+import Title from './components/Tiltle';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Tiltle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// functional component
+const Title = () => {
+    return(
+        <div>
+            <h2>Hello World コンポーネント</h2>
+        </div>
+    )
+}
+
+export default Title;
